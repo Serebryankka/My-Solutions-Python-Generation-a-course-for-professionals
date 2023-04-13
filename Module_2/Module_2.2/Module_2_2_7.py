4f47c09cabe9ea89e149f79bebf13b91539a65fe
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-14 02:24:42 +0300

    Create Module_2_2_7.py

---
+++ Module_2/Module_2.2/Module_2_2_7.py
@@ -0,0 +1,24 @@
+def task():
+    # word = input()
+    # words = (input() for _ in range(int(input())))
+    vowels = 'ауоыиэяюёе'
+    indexes = [i for i in range(len(word)) if word[i] in vowels]
+    count_indexes = len(indexes)
+    result = []
+    for w in words:
+        count = 0
+        for i in range(len(w)):
+            if w[i] in vowels:
+                if i not in indexes:
+                    break
+                else:
+                    count += 1
+        if count == count_indexes:
+            result.append(w)
+
+    return result
+
+
+if __name__ == '__main__':
+    print(*task(), sep='\n')
+    
