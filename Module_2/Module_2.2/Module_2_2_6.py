5e5638cc85f9783233743c4569821d6f5df86285
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-13 23:01:21 +0300

    Create Module_2_2_6.py

---
+++ Module_2/Module_2.2/Module_2_2_6.py
@@ -0,0 +1,8 @@
+def task():
+    data = (input() for _ in range(int(input())))
+    data = list(map(lambda x: x.split(', '), data))
+    result = set(data[0]).intersection(*data[1:])
+    return 'Сериал снять не удастся' if not result else ", ".join(sorted(result))
+
+if __name__ == '__main__':
+    print(task())
