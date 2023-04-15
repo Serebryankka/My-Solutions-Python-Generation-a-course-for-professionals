1ed3a881c179e9461e5fc953badd34d0f80f92a3
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-16 02:04:34 +0300

    Update Module_3_12.py

--- Module_2/Module_3/Module_3_12.py
+++ Module_2/Module_3/Module_3_12.py
@@ -4,5 +4,5 @@ from datetime import date
 def main(data):
     data1, data2 = data
     return min(date.fromisoformat(data1), date.fromisoformat(data2)).strftime('%d-%m (%Y)')
-print(main((input() for _ in range(2)))
+print(main([input() for _ in range(2)])
   
