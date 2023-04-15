d135af4f3135bd551852bc4dcd07348a130d03fa
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-16 02:04:13 +0300

    Create Module_3_12.py

---
+++ Module_2/Module_3/Module_3_12.py
@@ -0,0 +1,8 @@
+from datetime import date
+
+
+def main(data):
+    data1, data2 = data
+    return min(date.fromisoformat(data1), date.fromisoformat(data2)).strftime('%d-%m (%Y)')
+print(main((input() for _ in range(2)))
+  
