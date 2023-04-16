ff8e78a3cbc2d0302aac1c573dbc87fd4d47ddd9
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-16 16:55:49 +0300

    Update Module_3_2_16.py

--- Module_3/Module_3_2/Module_3_2_16.py
+++ Module_3/Module_3_2/Module_3_2_16.py
@@ -1,18 +1,23 @@
 from datetime import date
 
 
-def is_correct(dat):
-    day, month, year = map(int, dat.split('.'))
+def is_correct(d):
+    day, month, year = map(int, d.split('.'))
     try:
         date(year, month, day)
         return True
     except ValueError:
         return False
 
-def main(data):
+def main():
+    a = input()
+    data = []
+    while a != 'end':
+        data.append(a)
+        a = input()
     count = 0
     result = []
-    for d in data[:-1]:
+    for d in data:
         if is_correct(d):
             count += 1
             result.append('Корректная')
@@ -20,4 +25,6 @@ def main(data):
             result.append('Некорректная')
     result.append(str(count))
     return '\n'.join(result)
+if __name__ == '__main__':
+    print(main())
     
