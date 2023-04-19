4df220d37afd47212613d6807c3079dcbc8a84d7
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-19 19:44:55 +0300

    Update Module_3_5_7.py

--- Module_3/Module_3_5/Module_3_5_7.py
+++ Module_3/Module_3_5/Module_3_5_7.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 def main():
     today = datetime.strptime(input()[:-5], '%d.%m')
     seven_days = today + timedelta(weeks=1)
+    if seven_days.year > today.year:
+        today, seven_days = (seven_days + timedelta(days=-2)).replace(year=1900), today
     data = list(filter(lambda x: today < datetime(year=1900, month=x[1].month, day=x[1].day) <= seven_days,
                        map(lambda x: [x[0], datetime.strptime(x[1], '%d.%m.%Y')],
                            map(lambda x: x.rsplit(maxsplit=1), (input() for _ in range(int(input())))))))
