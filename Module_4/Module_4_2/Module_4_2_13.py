0b9d4e3e5be83edc22044c3605c2e27e1cd7bef4
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-21 17:10:22 +0300

    Update Module_4_2_13.py

--- Module_4/Module_4_2/Module_4_2_13.py
+++ Module_4/Module_4_2/Module_4_2_13.py
@@ -4,10 +4,9 @@ from collections import defaultdict
 
 with open('salary_data.csv', 'r', encoding='utf-8') as file:
     rows = csv.DictReader(file, delimiter=';')
-    d = defaultdict(lambda:defaultdict(int))
+    d = defaultdict(lambda: [0, 0])
     for row in rows:
-        d[row['company_name']]['salary'] += int(row['salary'])
-        d[row['company_name']]['count'] += 1
-    for row in sorted(d.items(), key=lambda item: item[1]['salary'] / item[1]['count']):
+        d[row['company_name']][0] += int(row['salary'])
+        d[row['company_name']][1] += 1
+    for row in sorted(d.items(), key=lambda item: item[1][0] / item[1][1]):
         print(row[0])
-      
