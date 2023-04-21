0122a8b82fc427ec1da92796fa5ccb3b154b9205
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-21 19:06:42 +0300

    Update Module_4_2_15.py

--- Module_4/Module_4_2/Module_4_2_15.py
+++ Module_4/Module_4_2/Module_4_2_15.py
@@ -10,6 +10,6 @@ with open('data.csv', 'r', encoding='utf-8') as csv_file:
 with open('domain_usage.csv', 'w', encoding='utf-8', newline='') as csv_file_res:
     writer = csv.DictWriter(csv_file_res, fieldnames=['domain', 'count'])
     writer.writeheader()
-    for k, v in sorted(d.items(), key=lambda x: x[1]):
+    for k, v in sorted(d.items(), key=lambda x: (x[1], x[0])):
         writer.writerow({'domain': k, 'count': v})
       
