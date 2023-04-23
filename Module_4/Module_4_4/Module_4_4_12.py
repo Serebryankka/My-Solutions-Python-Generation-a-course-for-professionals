52bf9710f389525d12177de929ea8a08b75dfbf3
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-24 01:58:35 +0300

    Create Module_4_4_12.py

---
+++ Module_4/Module_4_4/Module_4_4_12.py
@@ -0,0 +1,16 @@
+import json
+import csv
+
+
+with open('students.json', 'r', encoding='utf-8') as f:
+    result = []
+    for d in json.load(f):
+        #dictionary = defaultdict()
+        if int(d['age']) >= 18 and int(d['progress']) >= 75:
+            result.append({'name': d['name'], 'phone': d['phone']})
+    result.sort(key=lambda x: x['name'])
+with open('data.csv', 'w', encoding='utf-8', newline='') as f:
+    writer = csv.DictWriter(f, fieldnames=['name', 'phone'])
+    writer.writeheader()
+    writer.writerows(result)
+    
