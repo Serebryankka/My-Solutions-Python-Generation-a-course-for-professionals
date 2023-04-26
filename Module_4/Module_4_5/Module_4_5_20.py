c6c872a5f347dab0f583c1d8e09f9edad8af4676
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-26 14:20:23 +0300

    Create Module_4_5_20.py

---
+++ Module_4/Module_4_5/Module_4_5_20.py
@@ -0,0 +1,12 @@
+from zipfile import ZipFile
+import os.path
+
+
+file_names = ['how to prove.pdf', 'fipi_demo_2022.pdf', 'Hollow Knight Silksong.exe',
+              'code.jpeg', 'stepik.png', 'readme.txt', 'shopping_list.txt',
+              'Alexandra Savior – Crying All the Time.mp3', 'homework.py','test.py']
+with ZipFile('files.zip', 'w') as zip_file:
+    for file_name in file_names:
+        if os.path.getsize(file_name) <= 100:
+            zip_file.write(file_name)
+            
