66a0593e5212fa0cdcb9a8f6731cf5a5d743fa9a
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-04-27 15:43:04 +0300

    Create Module_6_4_11.py

---
+++ Module_6/Module_6_4/Module_6_4_11.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+
+
+User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
+
+users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+         User('Tracy', 'Wallace', 'sblack@m``````````````````````````````````e.com', 'Silver'),
+         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+
+prior = {'Gold': 0, 'Silver': 1, 'Bronze': 2, 'Basic': 3}
+for user in sorted(users, key=lambda x: (prior[x.plan], x.email)):
+    print(f'''{user.name} {user.surname}
+  Email: {user.email}
+  Plan: {user.plan}''')
+    print()
+    
