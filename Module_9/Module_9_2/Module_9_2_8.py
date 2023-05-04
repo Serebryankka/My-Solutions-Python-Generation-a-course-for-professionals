73681d1ee1a3c2f269ae9d43adea834cec3cdd30
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-05-04 16:50:33 +0300

    Create Module_9_2_8.py

---
+++ Module_9/Module_9_2/Module_9_2_8.py
@@ -0,0 +1,8 @@
+def hash_as_key(objects):
+    d = defaultdict(list)
+    [d[hash(i)].append(i) for i in objects]
+    for k, v in d.items():
+        if len(v) == 1:
+            d[k] = v[0]
+    return dict(d)
+  
