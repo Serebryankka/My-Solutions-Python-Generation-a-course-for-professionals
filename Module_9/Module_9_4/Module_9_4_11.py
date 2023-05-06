37f096ccbc2f5eb56bd8011e9cc13ab2edd69623
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-05-06 22:03:55 +0300

    Create Module_9_4_11.py

---
+++ Module_9/Module_9_4/Module_9_4_11.py
@@ -0,0 +1,4 @@
+def numbers_sum(elems):
+    '''Принимает список и возвращает сумму его чисел (int, float),
+игнорируя нечисловые объекты. 0 - если в списке чисел нет.'''
+    return sum(elem for elem in elems if isinstance(elem, (int, float)))
