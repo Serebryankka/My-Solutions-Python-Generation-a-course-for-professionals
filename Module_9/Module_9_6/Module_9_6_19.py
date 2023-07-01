2ca03471c95f6c2768d1f6d7023f2c92318991f2
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-07-01 15:13:07 +0300

    Create Module_9_6_19.py

---
+++ Module_9/Module_9_6/Module_9_6_19.py
@@ -0,0 +1,8 @@
+def sandwich(func):
+    def wrapper(*args, **kwargs):
+        print('---- Верхний ломтик хлеба ----')
+        res = func(*args, **kwargs)
+        print('---- Нижний ломтик хлеба ----')
+        return res
+    return wrapper
+  
