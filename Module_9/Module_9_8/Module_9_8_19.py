bf58a04c5eb42adfcf84a2263aaaa8342cc5c3a5
Serebryankka <130122744+Serebryankka@users.noreply.github.com>
2023-07-21 17:15:31 +0300

    Create Module_9_8_19.py

---
+++ Module_9/Module_9_8/Module_9_8_19.py
@@ -0,0 +1,13 @@
+import functools
+
+
+def repeat(times):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(times):
+                value = func(*args, **kwargs)
+            return value
+        return wrapper
+    return decorator
+  
